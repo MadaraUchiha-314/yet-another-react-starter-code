eff07c2a2149ab750c0229e223827560475cb675
MadaraUchiha-314 <rohithr31@gmail.com>
2019-09-03 11:12:49 +0530

    Random comment changes

--- src/js/widgets/homepage/Hello.tsx
+++ src/js/widgets/homepage/Hello.tsx
@@ -5,8 +5,10 @@ export interface HelloProps {
     framework: string;
 }
 
-// 'HelloProps' describes the shape of props.
-// State is never set so we use the '{}' type.
+/*
+* 'HelloProps' describes the shape of props.
+* State is never set so we use the '{}' type.
+*/
 export class Hello extends React.Component<HelloProps, {}> {
   render() {
     return (
